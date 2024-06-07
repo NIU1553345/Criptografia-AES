b55a095f63ad66a77785678576e6d8aeabf4d5b6
NIU1553345 <91468777+NIU1553345@users.noreply.github.com>
2024-06-07 18:43:16 +0200

    Arreclar ex 1,2,3

--- hash/hash.py
+++ hash/hash.py
@@ -1,54 +1,42 @@
 import hashlib
 from typing import Optional, Tuple
 
-def uab_md5_explica(message: str, num_bits: int) -> Optional[int]:
-    print("message: ", message)
-    message1 = message.encode()
-    print("message encoded: ", message1)
-    message2 = hashlib.md5(message1)
-    print("message hashed: ", message2)
-    message3 = message2.hexdigest()
-    print("message hexdigest: ", message3)
-    message4 = message3[:num_bits // 4]
-    print("message truncated: ", message4)
-    message5 = int(message4, 16)
-    print("message int: ", message5)
-    return message5
-
 def uab_md5(message: str, num_bits: int) -> Optional[int]:
-    return int(hashlib.md5(message.encode()).hexdigest()[:num_bits], 16)
-
-print(uab_md5("hello", 23) ) # 907060870
-
-print(uab_md5_explica("hello", 16) )
-
-def second_preimage_md5(message: str, num_bits: int) -> Optional[str]:
-    primera_imatge = uab_md5(message, num_bits)
-    for i in range(1000000):
-        segona_imatge = uab_md5(message + str(i), num_bits)
-        if primera_imatge == segona_imatge:
-            return message + str(i)
+    if 1 <= num_bits <= 128:
+        hash_hexa = hashlib.md5(message.encode()).hexdigest()
+        hash_bin = bin(int(hash_hexa, 16))[2:].zfill(128)
+        return int(hash_bin[:num_bits], 2)
     return None
 
-print(second_preimage_md5("hello", 16))
+print("1r: ",uab_md5("hello", 16) ) 
+print
+def second_preimage(message: str, num_bits: int) -> Optional[Tuple[str, int]]:
+    primera_imatge = uab_md5(message, num_bits)
+    for i in range(1000000):
+        segona_imatge = uab_md5(str(i), num_bits)
+        if primera_imatge == segona_imatge:
+            return (str(i), i)
+    return None
+
+print("2n: ",second_preimage("hello", 16))
 
 print(uab_md5("hello", 16) ) 
 print(uab_md5("hello81259", 16)) 
 
-def collision_md5(num_bits: int) -> Tuple[str, str]:
+def collision(num_bits: int) -> Optional[Tuple[str, str, int]]:
     hashes = {}
     for i in range(1000000):
         message1 = str(i)
         hash1 = uab_md5(message1, num_bits)
         if hash1 in hashes:
-            return message1, hashes[hash1], hash1
+            return (message1, hashes[hash1], i)
         hashes[hash1] = message1
-    return None, None, None
+    return None
 
-print(collision_md5(32))
+print(collision(32))
 
-# for i in range (8, 96, 8):
-#     print(i, "bits:")
-#     print("second preimage: ", second_preimage_md5("hello", i))
-#     print("collision: ", collision_md5(i))
+for i in range (8, 96, 8):
+    print(i, "bits:")
+    print("second preimage: ", second_preimage("hello", i))
+    print("collision: ", collision(i))
     
