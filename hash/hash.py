9e2af0fb7857304d4840f7749b1ca1583010b510
NIU1553345 <91468777+NIU1553345@users.noreply.github.com>
2024-06-10 21:25:13 +0200

    Finale

--- hash/hash.py
+++ hash/hash.py
@@ -17,7 +17,7 @@ def uab_md5(message: str, num_bits: int) -> Optional[int]:
 
 def second_preimage(message: str, num_bits: int) -> Optional[Tuple[str, int]]:
     primera_imatge = uab_md5(message, num_bits)
-    for i in range(1000000):
+    for i in range(100000000):
         segona_imatge = uab_md5(str(i), num_bits)
         if primera_imatge == segona_imatge:
             return (str(i), i)
@@ -30,7 +30,7 @@ def second_preimage(message: str, num_bits: int) -> Optional[Tuple[str, int]]:
 
 def collision(num_bits: int) -> Optional[Tuple[str, str, int]]:
     hashes = {}
-    for i in range(1000000):
+    for i in range(100000000):
         message1 = str(i)
         hash1 = uab_md5(message1, num_bits)
         if hash1 in hashes:
@@ -45,43 +45,33 @@ def exercici_4A():
     resultats = []
 
     for i in range (1, 25):
-
-        print(i, "bits:")
         start_time_feble = time.perf_counter()
-
-        print("second preimage: ", second_preimage("hello", i))
         _, iteracions_feble =  second_preimage("hello", i)
-
         elapsed_time_feble = time.perf_counter() - start_time_feble
-        print("Elapsed time second preimage: ", elapsed_time_feble)
 
         start_time_forta = time.perf_counter()
-
-        print("collision: ", collision(i))
         _, _, iteracions_forta = collision(i)
-
         elapsed_time_forta = time.perf_counter() - start_time_forta
-        print("Elapsed time collision: ", elapsed_time_forta)
     
         resultats.append((i, elapsed_time_feble, iteracions_feble, elapsed_time_forta, iteracions_forta))
 
     df_results = pd.DataFrame(resultats, columns=["Bits", "Temps Col·lisió Feble", "Iteracions Col·lisió Feble", "Temps Col·lisió Forta", "Iteracions Col·lisió Forta"])
     
     print(df_results)
-
-    # Gràfica de temps: mostra el temps que es triga per obtenir un resultat en funci´o del nombre de bits en cada cas.
+##################################################################### 
     plt.plot(df_results["Bits"], df_results["Temps Col·lisió Feble"], label="Col·lisió Feble")
     plt.plot(df_results["Bits"], df_results["Temps Col·lisió Forta"], label="Col·lisió Forta")
     plt.xlabel("Bits")
     plt.ylabel("Temps")
+    plt.title("Temps per acabar l'execució en funció del nombre de bits")
     plt.legend()
     plt.show()
 
-    #  Gràfica de iteracions: mostra el nombre de iteracions o valors que s’han hagut de generar en funci´o del nombre de bits per cada cas.
     plt.plot(df_results["Bits"], df_results["Iteracions Col·lisió Feble"], label="Col·lisió Feble")
     plt.plot(df_results["Bits"], df_results["Iteracions Col·lisió Forta"], label="Col·lisió Forta")
     plt.xlabel("Bits")
     plt.ylabel("Iteracions")
+    plt.title("Iteracions per obtenir la col·lisió en funció del nombre de bits") 
     plt.legend()
     plt.show()
 
@@ -92,7 +82,7 @@ def exercici_4A():
 def exercici4B(df_results):
 
     def iteracions_teoriques_fortes(bits):
-        return int(math.ceil(2 ** (bits / 2)))
+        return math.ceil(2 ** (bits / 2))
 
     def iteracions_teoriques_febles(bits):
         return math.ceil(2 ** bits)
@@ -110,8 +100,6 @@ def exercici4B(df_results):
     # Gràfica de comparació del nombre de iteracions reals i teòriques per la col·lisió feble.
     plt.plot(df_results["Bits"], df_results["Iteracions Col·lisió Feble"], label="Iteracions Reals")
     plt.plot(df_results["Bits"], df_results["Iteracions Teòriques Feble"], label="Iteracions Teòriques")
-    plt.plot(df_results["Bits"], df_results["Iteracions Col·lisió Forta"], label="Iteracions Reals")
-    plt.plot(df_results["Bits"], df_results["Iteracions Teòriques Forta"], label="Iteracions Teòriques")
     plt.xlabel("Bits")
     plt.ylabel("Iteracions")
     plt.title("Comparació de iteracions Reals amb Teòriques de les col·lision Febles")
